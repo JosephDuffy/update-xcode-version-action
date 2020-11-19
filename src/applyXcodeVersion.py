d535dc4f220bafde62e09c7014ec6d7746914259
Joseph Duffy <commits@josephduffy.co.uk>
2020-11-19 14:56:50 +0000

    Add --yaml_file flag to read/write file directly

--- src/applyXcodeVersion.py
+++ src/applyXcodeVersion.py
@@ -20,6 +20,11 @@ parser = argparse.ArgumentParser(description="Apply Xcode version to YAML file")
 parser.add_argument("yaml_key", nargs="+", help="Key in YAML file to update")
 # TODO: Accept an array
 parser.add_argument("yaml_value", type=str, help="Value to set key to")
+parser.add_argument(
+    "--yaml_file",
+    type=str,
+    help="The file to update. If not provided will read from stdin and write to stdout",
+)
 
 args = parser.parse_args()
 
@@ -29,8 +34,18 @@ value = args.yaml_value
 yaml = YAML()
 yaml.preserve_quotes = True
 yaml.indent(mapping=2, sequence=4, offset=2)
-file = yaml.load(sys.stdin)
+
+yaml_file = None
+
+if args.yaml_file:
+    yaml_file = pathlib.Path(args.yaml_file)
+    file = yaml.load(yaml_file)
+else:
+    file = yaml.load(sys.stdin)
 
 setInDict(file, key, value)
 
-yaml.dump(file, sys.stdout)
+if yaml_file:
+    yaml.dump(file, yaml_file)
+else:
+    yaml.dump(file, sys.stdout)
