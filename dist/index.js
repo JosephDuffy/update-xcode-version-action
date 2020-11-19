78e65c1b2e8d9b853679ae951ae4c2914f33a1d1
Joseph Duffy <commits@josephduffy.co.uk>
2020-11-19 19:32:59 +0000

    Run `npm run pack`

--- dist/index.js
+++ dist/index.js
@@ -3106,15 +3106,20 @@ async function run() {
         await applyXcodeVersionsFile_1.default(xcodeVersionsFilePath, xcutilsVersionResolver);
         const githubToken = core.getInput("github-token");
         if (githubToken !== "") {
+            core.info("Have a GitHub token; creating pull request");
             await exec_1.exec("git", [
                 "checkout",
                 "-b",
                 "update-xcode-version-action/update-xcode-versions",
             ]);
+            core.info("Created branch");
             await exec_1.exec("git", ["add", "."]);
+            core.info("Stages all changes");
             await exec_1.exec("git", ["commit", "-m", "Update Xcode Versions"]);
+            core.info("Created commit");
             const octokit = github.getOctokit(githubToken);
             await octokit.pulls.create();
+            core.info("Created octokit");
         }
     }
     catch (error) {
